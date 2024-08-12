c482c6b46a413dd9aba48b5b6541dda6e255968c
William Guilherme <wguilherme@securitygeek.io>
2024-08-12 10:42:45 -0700

    fix: Fixed cholateteyinstall.ps1 script environment variable install location

--- zscaler-terraformer/tools/chocolateyinstall.ps1
+++ zscaler-terraformer/tools/chocolateyinstall.ps1
@@ -3,32 +3,41 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 # Define package details
 $packageName = 'zscaler-terraformer'
-$zipFile = "$toolsDir\zscaler-terraformer_1.3.0_windows_amd64.zip"
 $unzipLocation = "$toolsDir\zscaler-terraformer"
 
 # URL and checksum for the .zip file
 $url = 'https://github.com/zscaler/zscaler-terraformer/releases/download/v1.3.0/zscaler-terraformer_1.3.0_windows_amd64.zip'
 $checksum = '84408b4496a87298d70d955f3c300571ba5ed7559ab3d967ef02e6efe41e5fd0'
 
-# Download the zip file
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $zipFile -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
-
-# Unzip the package
+# Download, verify, and unzip the package
 $packageArgs = @{
     packageName    = $packageName
-    FileFullPath   = $zipFile
-    Destination    = $unzipLocation
+    unzipLocation  = $unzipLocation
+    fileType       = 'zip'
+    url            = $url
+    checksum       = $checksum
+    checksumType   = 'sha256'
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
 # Move the executable to a directory in the system PATH
 $exePath = "$unzipLocation\zscaler-terraformer.exe"
 $destinationPath = "$Env:ProgramFiles\zscaler-terraformer"
+
+# Ensure the destination directory exists
 if (-not (Test-Path $destinationPath)) {
     New-Item -Path $destinationPath -ItemType Directory -Force
 }
+
+# Check if the file already exists and remove it if necessary
+$destinationFile = "$destinationPath\zscaler-terraformer.exe"
+if (Test-Path $destinationFile) {
+    Remove-Item -Path $destinationFile -Force
+}
+
+# Move the executable
 Move-Item -Path $exePath -Destination $destinationPath
 
-# Clean up the zip file
-Remove-Item -Path $zipFile -Force -ErrorAction SilentlyContinue 
+# Clean up the temporary directory
+Remove-Item -Recurse -Force $unzipLocation -ErrorAction SilentlyContinue
