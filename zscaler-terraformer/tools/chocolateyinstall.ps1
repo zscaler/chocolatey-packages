6eaff83a1b103764511e88a7751d9d95c1e4be53
William Guilherme <wguilherme@securitygeek.io>
2025-08-22 16:44:23 -0700

    fix: Release v2.0.15

--- zscaler-terraformer/tools/chocolateyinstall.ps1
+++ zscaler-terraformer/tools/chocolateyinstall.ps1
@@ -17,12 +17,12 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'zscaler-terraformer'
 
 # URL and checksum for the 64-bit .zip file
-$url64bit = 'https://github.com/zscaler/zscaler-terraformer/releases/download/v2.0.14/zscaler-terraformer_2.0.14_windows_amd64.zip'
-$checksum64bit = 'bb8987cc630268782d060373056cd6930ace358f54db1e320abf9553ca93bb18'
+$url64bit = 'https://github.com/zscaler/zscaler-terraformer/releases/download/v2.0.15/zscaler-terraformer_2.0.15_windows_amd64.zip'
+$checksum64bit = 'd2c2bd950f76cc45a59c97def390e707b0e566f3f0f1b1831ccf0096fc67880e'
 
 # URL and checksum for the 32-bit .zip file
-$url32bit = 'https://github.com/zscaler/zscaler-terraformer/releases/download/v2.0.14/zscaler-terraformer_2.0.14_windows_386.zip'
-$checksum32bit = 'e33fa04782aa448d34cb71554956a29943fc9134313b249737786a35fe6887b8'
+$url32bit = 'https://github.com/zscaler/zscaler-terraformer/releases/download/v2.0.15/zscaler-terraformer_2.0.15_windows_386.zip'
+$checksum32bit = '0077aa5d1a495e1d7ff6f7aaf71757ce9aa5610aab2325005833c7ccee876410'
 
 # Download, verify, and unzip the package
 $packageArgs = @{
